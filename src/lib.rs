be8f2d366d8a95731a59faf3b6286a706505f32b
gabe <gnmcd13485@gmail.com>
2024-05-24 22:25:42 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+/*
+Attempt the same thing, but with nested traits
+
+The idea being that inheritance can be approximated by an object holding an instance of the object
+it "inherits" from
+ */
+struct APM{}
+
+struct AEM{}
+struct OPM{}
+struct OMM{}
+struct OEM{}
+struct OCM{}
+struct TDM{}
+struct CDM{}
+struct RDM{}
+
+trait SingleSegment{}
+trait MultiSegment{}
